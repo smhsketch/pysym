ade1f621d303a00b6b546ca59507e493f74342d0
bruh <phwarren3@gmail.com>
2020-03-22 23:13:04 -0500

    make sympies file

--- sympy.py
+++ sympy.py
@@ -4,18 +4,25 @@ import sys
 # example:
 # sympy create ~/picture.jpg ~/Pictures/Family/picture.jpg
 # ^ will create a symlink to ~/picture.jpg in the ~/Pictures/Family directory
-# sympy will also log this symlink in the sympies file (~/.config/sympy/
-# 
-# sympies) so the symlink can be easily cloned or deleted later
+# sympy will also log this symlink in the sympies file (/etc/sympies) so the symlink can be easily cloned or deleted later
+user = (os.environ["USER"])
+# print(user)
+print(user)
+try:
+    syms = open("/home/"+user+"/.config/sympies", "r")
+    syms.close()
+    print(syms)
+except FileNotFoundError:
+    print("there is no sympies file. creating...")
+    os.system("touch ~/.config/sympies")
 
-print(sys.argv)
 
 def create():
     print("creating symlink...")
     os.system("ln -s "+sys.argv[2]+" "+sys.argv[3])
 
 if len(sys.argv) > 1: # look for arguments like create, delete, etc
-    if sys.argv[1] == "create":
+    if "create" in sys.argv:
         try: # checks to make sure the file to be symlinked exists
             check = open(sys.argv[2])
             check.close()
@@ -29,5 +36,8 @@ if len(sys.argv) > 1: # look for arguments like create, delete, etc
                 create()
         else:
             create()
+    elif "view" in sys.argv:
+            syms = open("~/.config/sympies", "r")
+            syms.close()
 
 # TODO - log a list of symlinks to a file so they can be removed/edited
