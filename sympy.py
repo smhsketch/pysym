8bf726ed1b759ac8af5619ff625b906982ac4d13
smhsketch <phwarren3@gmail.com>
2020-03-27 18:31:21 -0400

    changed sympies file to sympies folder

--- sympy.py
+++ sympy.py
@@ -1,20 +1,20 @@
+#!/usr/bin/python3
 import os
 import sys
 
 # example:
 # sympy create ~/picture.jpg ~/Pictures/Family/picture.jpg
 # ^ will create a symlink to ~/picture.jpg in the ~/Pictures/Family directory
-# sympy will also log this symlink in the sympies file (/etc/sympies) so the symlink can be easily cloned or deleted later
-user = (os.environ["USER"])
-# print(user)
-print(user)
+# sympy will also log this symlink in the sympies folder (~/.config/sympies/) so the symlink can be easily cloned or deleted later
+
+# user = (os.environ["USER"])
 try:
-    syms = open("/home/"+user+"/.config/sympies", "r")
-    syms.close()
-    print(syms)
+    symfile = open("~/.config/sympies/.sympy", "r")
+    symfile.close()
+    
 except FileNotFoundError:
     print("there is no sympies file. creating...")
-    os.system("touch ~/.config/sympies")
+    os.system("mkdir ~/.config/sympies; touch ~/.config/sympies/.sympy")
 
 
 def create():
@@ -22,7 +22,7 @@ def create():
     os.system("ln -s "+sys.argv[2]+" "+sys.argv[3])
 
 if len(sys.argv) > 1: # look for arguments like create, delete, etc
-    if "create" in sys.argv:
+    if sys.argv[1] == "create":
         try: # checks to make sure the file to be symlinked exists
             check = open(sys.argv[2])
             check.close()
@@ -32,12 +32,13 @@ if len(sys.argv) > 1: # look for arguments like create, delete, etc
                 exit()
             else:
                 os.system("touch "+sys.argv[2])
+                os.system("touch ~/.config/sympies/"+sys.argv[2])
+                os.system("echo $PWD"+sys.argv[3]+" >> ~/.config/sympies/"+sys.argv[2]) # 
                 print("created file "+sys.argv[2])
                 create()
         else:
             create()
-    elif "view" in sys.argv:
-            syms = open("~/.config/sympies", "r")
-            syms.close()
+    elif sys.argv[1] == "view":
+            pass
 
 # TODO - log a list of symlinks to a file so they can be removed/edited
