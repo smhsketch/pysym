900a3b3212d49422dd1751546723f05f71462d5f
smhsketch <phwarren3@gmail.com>
2020-03-27 21:43:22 -0400

    add full path support

--- pysym.py
+++ pysym.py
@@ -3,20 +3,23 @@ import os
 import sys
 
 # example:
-# sympy create ~/picture.jpg ~/Pictures/Family/picture.jpg
+# pysym create ~/picture.jpg ~/Pictures/Family/picture.jpg
 # ^ will create a symlink to ~/picture.jpg in the ~/Pictures/Family directory
-# sympy will also log this symlink in the pysyms folder (~/.config/pysym/) so the symlink can be easily cloned or deleted later
+# pysym will also log this symlink in the pysyms folder (~/.config/pysym/) so the symlink can be easily cloned or deleted later
 
-user = (os.environ["USER"])
+user = os.environ["USER"]
+pwd = os.environ["PWD"]
+version = "0.0.5"
 
 try:
-    symfile = open("/home/"+user+"/.config/sympies/pysym.version", "r") # python does not understand ~
+    symfile = open("/home/"+user+"/.config/pysyms/pysym.version", "r") # python does not understand ~
     symfile.readlines()
     symfile.close()
 
 except FileNotFoundError:
-    print("there is no sympies folder. creating...")
-    os.system("mkdir ~/.config/sympies && touch ~/.config/sympies/pysym.version")
+    print("there is no pysyms folder. creating...")
+    os.system("mkdir ~/.config/pysyms && touch ~/.config/pysyms/pysym.version")
+    os.system("echo "+version+" >> ~/.config/pysyms/pysym.version")
 
 def create():
     print("creating symlink...")
@@ -24,7 +27,7 @@ def create():
 
 if len(sys.argv) > 1: # look for arguments like create, delete, etc
     if sys.argv[1] == "version":
-        os.system("head ~/.config/sympies/pysym.version")
+        os.system("head ~/.config/pysyms/pysym.version")
     if sys.argv[1] == "create":
         try: # checks to make sure the file to be symlinked exists
             check = open(sys.argv[2])
@@ -35,13 +38,15 @@ if len(sys.argv) > 1: # look for arguments like create, delete, etc
                 exit()
             else:
                 os.system("touch "+sys.argv[2])
-                os.system("touch ~/.config/sympies/"+sys.argv[2]+".pysym")
-                os.system("echo "+sys.argv[3]+" | head -n 1 >> ~/.config/sympies/"+sys.argv[2]+".pysym") # FIXME
+                os.system("touch ~/.config/pysyms/"+sys.argv[2]+".pysym")
+                if sys.argv[3].startswith("/"): # make the link register the full path
+                    write = sys.argv[3]
+                else:
+                    write = pwd+"/"+sys.argv[3]
+                os.system("echo "+write+" | head -n 1 >> ~/.config/pysyms/"+sys.argv[2]+".pysym")
                 print("created file "+sys.argv[2])
                 create()
         else:
             create()
     elif sys.argv[1] == "view":
             pass
-
-# TODO - log a list of symlinks to a file so they can be removed/edited
